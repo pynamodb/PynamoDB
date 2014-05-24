7db03269a0fe1cfcd00991e42150d5707c90397a
Jharrod LaFon <jlafon@eyesopen.com>
2014-05-24 16:35:16 -0600

    New fully working example of a URL shortener that stores URLs in DynamoDB

---
+++ examples/url_shortener/README.rst
@@ -0,0 +1,25 @@
+==========================
+A very short URL shortener
+==========================
+
+This is a very small implementation of a `URL shortener <http://en.wikipedia.org/wiki/URL_shortening>`_ powered by Flask and PynamoDB.
+This example works with both Python 2 and Python 3.
+
+Try it for yourself in three easy steps, assuming you have `access to AWS <http://pynamodb.readthedocs.org/en/latest/awsaccess.html>`_.
+
+Install Requirements
+====================
+::
+
+    $ pip install flask pynamodb
+
+Run the server
+==============
+::
+
+    $ python shortener.py
+
+Shorten URLs
+============
+
+Now you can navigate to `http://localhost:5000 <http://localhost:5000>`_ to start shortening URLs.
---
+++ examples/url_shortener/shortener.py
@@ -0,0 +1,67 @@
+"""
+A fully working url shortener example
+"""
+from __future__ import print_function
+import flask
+from hashlib import md5
+from base64 import b64encode
+from pynamodb.models import Model
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+from pynamodb.attributes import UnicodeAttribute
+
+
+class Url(Model):
+    class Meta:
+        table_name = "shortened-urls"
+        host = "http://localhost:8000"
+
+    class CodeIndex(GlobalSecondaryIndex):
+        class Meta:
+            read_capacity_units = 1
+            write_capacity_units = 1
+            projection = AllProjection()
+        code = UnicodeAttribute(hash_key=True)
+
+    url = UnicodeAttribute(hash_key=True)
+    code = UnicodeAttribute()
+    code_index = CodeIndex()
+
+    def save(self, **kwargs):
+        """
+        Generates the shortened code before saving
+        """
+        self.code = b64encode(
+            md5(self.url.encode('utf-8')).hexdigest()[-4:].encode('utf-8')
+        ).decode('utf-8').replace('=', '').replace('/', '_')
+        super(Url, self).save(**kwargs)
+
+app = flask.Flask(__name__)
+app.config.update(DEBUG=True)
+
+@app.route('/')
+def index():
+    return flask.render_template("index.html")
+
+@app.route('/shorten/<path:url>')
+def shorten(url):
+    model = Url(url)
+    model.save()
+    return flask.Response(model.code)
+
+@app.route('/<path:code>')
+def resolve(code):
+    # next() in Python3 is __next__()
+    try:
+        model = Url.code_index.query(code).next()
+    except AttributeError:
+        model = Url.code_index.query(code).__next__()
+    except StopIteration:
+        flask.abort(404)
+    finally:
+        return flask.redirect(model.url)
+
+if __name__ == "__main__":
+    if not Url.exists():
+        print("Creating table...")
+        Url.create_table(wait=True, read_capacity_units=1, write_capacity_units=1)
+    app.run()
