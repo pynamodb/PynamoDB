203ac191392b79b4a88ba7dc45ab2508f43c99bd
John Liu <jliu@lyft.com>
2017-06-09 12:16:40 -0700

    missing pytest

--- pynamodb/tests/test_model.py
+++ pynamodb/tests/test_model.py
@@ -10,6 +10,7 @@ from datetime import datetime
 import six
 from botocore.client import ClientError
 from botocore.vendored import requests
+import pytest
 
 from pynamodb.compat import CompatTestCase as TestCase
 from pynamodb.tests.deep_eq import deep_eq
