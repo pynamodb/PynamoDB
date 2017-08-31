fcee93da74bee064d0fb0991e6d6b01a9229115a
Jeff Pinner <jpinner@lyft.com>
2017-08-30 17:22:12 -0700

    add doc for substitute_names

--- pynamodb/expressions/util.py
+++ pynamodb/expressions/util.py
@@ -7,6 +7,13 @@ def substitute_names(document_path, placeholders):
     """
     Replaces all attribute names in the given document path with placeholders.
     Stores the placeholders in the given dictionary.
+
+    :param document_path: list of path segments (an attribute name and optional list dereference)
+    :param placeholders:  a dictionary to store mappings from attribute names to expression attribute name placeholders
+
+    For example: given the document_path for some attribute "baz", that is the first element of a list attribute "bar",
+    that itself is a map element of "foo" (i.e. ['foo', 'bar[0], 'baz']) and an empty placeholders dictionary,
+    `substitute_names` will return "#0.#1[0].#2" and placeholders will contain {"foo": "#0", "bar": "#1", "baz": "#2}
     """
     for idx, segment in enumerate(document_path):
         match = PATH_SEGMENT_REGEX.match(segment)
