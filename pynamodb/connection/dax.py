6899e9f40b5fc19fc4d9ad3657cfb00bb883b79e
opapy <opapy.worker@gmail.com>
2018-02-27 19:46:24 +0900

    rmeove connection ppling

--- pynamodb/connection/dax.py
+++ pynamodb/connection/dax.py
@@ -1,11 +1,5 @@
 # coding: utf-8
-from hashlib import sha256
-
 from amazondax import AmazonDaxClient
-
-
-def get_connection_hash(endpoints):
-    return sha256(''.join(endpoints).encode('utf-8')).hexdigest()
 
 
 class DaxClient(object):
@@ -21,21 +15,9 @@ class DaxClient(object):
         'BatchWriteItem': 'batch_write_item',
     }
 
-    _connections = {}
-
     def __init__(self, session, endpoints):
-        self.session = session
-        self.endpoints = endpoints
-
-    @classmethod
-    def _get_connection(cls, session, endpoints):
-        connection_hash = get_connection_hash(endpoints)
-
-        if connection_hash not in cls._connections.keys():
-            cls._connections[connection_hash] = AmazonDaxClient(session, endpoints=endpoints)
-
-        return cls._connections[connection_hash]
+        self.connection = AmazonDaxClient(session, endpoints=endpoints)
 
     def dispatch(self, operation_name, kwargs):
-        method = getattr(self._get_connection(self.session, self.endpoints), self.OP_NAME_TO_METHOD[operation_name])
+        method = getattr(self.connection, self.OP_NAME_TO_METHOD[operation_name])
         return method(**kwargs)
