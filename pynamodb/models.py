a81605b29195f587f9326f9cd263ad80525846c1
opapy <opapy.worker@gmail.com>
2018-03-01 15:36:52 +0900

    fix rebase problem

--- pynamodb/models.py
+++ pynamodb/models.py
@@ -1294,12 +1294,9 @@ class Model(AttributeContainer):
                                               request_timeout_seconds=cls.Meta.request_timeout_seconds,
                                               max_retry_attempts=cls.Meta.max_retry_attempts,
                                               base_backoff_ms=cls.Meta.base_backoff_ms,
-<<<<<<< HEAD
                                               aws_access_key_id=cls.Meta.aws_access_key_id,
-                                              aws_secret_access_key=cls.Meta.aws_secret_access_key)
-=======
+                                              aws_secret_access_key=cls.Meta.aws_secret_access_key,
                                               dax_endpoints=cls.Meta.dax_endpoints)
->>>>>>> add dax_endpoints params
         return cls._connection
 
     def _deserialize(self, attrs):
