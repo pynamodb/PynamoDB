70c341e8e28ebc8c2a7af742dbc0217e581aa94f
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2018-12-31 11:56:24 -0800

    simply "reconstruct" the comments

--- pynamodb/tests/mypy_helpers.py
+++ pynamodb/tests/mypy_helpers.py
@@ -1,40 +1,40 @@
 import re
 import tempfile
-from collections import namedtuple
+from collections import defaultdict
 from textwrap import dedent
+from typing import Dict, List, Iterable
 
 import pytest
 
 mypy_api = pytest.importorskip("mypy.api")
 
 
-_MypyError = namedtuple('_MypyError', 'line_no error')
-
-
-def _run_mypy(program):
+def _run_mypy(program):  # type: (str) -> Iterable[str]
     with tempfile.TemporaryDirectory() as tempdirname:
         with open('{}/__main__.py'.format(tempdirname), 'w') as f:
-            f.write(dedent(program))
+            f.write(program)
         error_pattern = re.compile(r'^{}:(\d+): error: (.*)$'.format(re.escape(f.name)))
         stdout, stderr, exit_status = mypy_api.run([f.name])
+
+        # Group errors by line
+        errors_by_line: Dict[int, List[str]] = defaultdict(list)
         for line in stdout.split('\n'):
             m = error_pattern.match(line)
             if m:
-                yield _MypyError(line_no=int(m.group(1)), error=m.group(2))
+                errors_by_line[int(m.group(1))].append(m.group(2))
+
+        # Reconstruct the "actual" program with "error" comments
+        pattern_optional_comment = re.compile(r'^(.*?)(\s+# E: .*)?$')
+        for line_no, line in enumerate(program.split('\n'), start=1):
+            m = pattern_optional_comment.match(line)
+            errors = errors_by_line.get(line_no)
+            if errors:
+                yield '{}{}'.format(m.group(1), ''.join('  # E: {}'.format(error) for error in errors))
+            else:
+                yield m.group(1)
 
 
-def _parse_expected_mypy_errors(program):
-    error_pattern = re.compile(r'# E: (.+?)(?=\s*#|$)')  # matches every error expected on a given line
-    for line_no, line in enumerate(program.split('\n'), start=1):
-        for error in error_pattern.findall(line):
-            yield _MypyError(line_no=line_no, error=error)
-
-
-def _repr_mypy_errors(errors):
-    return '\n'.join('Line {}: {}'.format(error.line_no, error.error) for error in errors)
-
-
-def assert_mypy_output(program):
-    actual = _run_mypy(program)
-    expected = _parse_expected_mypy_errors(program)
-    assert _repr_mypy_errors(actual) == _repr_mypy_errors(expected)
+def assert_mypy_output(program):  # type: (str) -> None
+    program = dedent(program).strip()
+    actual = '\n'.join(_run_mypy(program))
+    assert actual == program
