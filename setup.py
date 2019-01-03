daabcf9bba6981a510ec1086f6aad6c6935cab40
kritchie <ritchie.karl@gmail.com>
2019-01-03 04:00:59 -0500

    Updates setup.py and tox.ini to reflect supported python versions (#544)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'Programming Language :: Python',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
     ],
     extras_require={
