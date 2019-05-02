de4702db2afa00a7e775f04c03dd74153c72d3fd
Hallie Lomax <hallie@lyft.com>
2019-05-01 22:37:06 -0700

    adding service name

--- pynamodb/tests/integration/test_transaction_integration.py
+++ pynamodb/tests/integration/test_transaction_integration.py
@@ -46,7 +46,7 @@ for model in [User, BankStatement, LineItem]:
         model.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
 
 transact_write = TransactWrite()
-print(transact_write._connection.session.get_service_model()._service_description)
+print(transact_write._connection.session.get_service_model(service_name='dynamodb')._service_description)
 User(1).save(in_transaction=transact_write)
 BankStatement(1).save(condition=(BankStatement.user_id.does_not_exist()), in_transaction=transact_write)
 transact_write.commit()
