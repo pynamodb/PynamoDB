71afc29be45416ef8bd31013ae2a393edde3ada6
Hallie Lomax <hallie@lyft.com>
2019-05-02 09:30:58 -0700

    adding host and region

--- pynamodb/tests/integration/test_transaction_integration.py
+++ pynamodb/tests/integration/test_transaction_integration.py
@@ -45,12 +45,12 @@ for model in [User, BankStatement, LineItem]:
         print("Creating table for model {0}".format(model.Meta.table_name))
         model.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
 
-transact_write = TransactWrite()
+transact_write = TransactWrite(host=cfg.DYNAMODB_HOST, region='us-east-1')
 User(1).save(in_transaction=transact_write)
 BankStatement(1).save(condition=(BankStatement.user_id.does_not_exist()), in_transaction=transact_write)
 transact_write.commit()
 
-transact_get = TransactGet()
+transact_get = TransactGet(host=cfg.DYNAMODB_HOST, region='us-east-1')
 User.get(1, in_transaction=transact_get)
 BankStatement.get(1, in_transaction=transact_get)
 user, statement = transact_get.commit()
