24b3c0bc6dbfc9295882ac009f3af1da781e03cd
Regina <reginalin714@gmail.com>
2019-11-13 11:22:12 -0500

    support python 2

--- pynamodb/_compat.py
+++ pynamodb/_compat.py
@@ -2,7 +2,9 @@ import six
 
 if six.PY2:
     from inspect import getargspec as getfullargspec
+    from imp import load_source
 else:
     from inspect import getfullargspec
+    from importlib.machinery import SourceFileLoader
 
 __all__ = ('getfullargspec',)
