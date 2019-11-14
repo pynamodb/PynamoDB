9d1f66fd81a8adce7c9d300e3b703a67155b818f
Regina Lin <reginalin714@gmail.com>
2019-11-14 01:06:18 -0600

    use importlib instead of deprecated imp (#723)
    On Python>=3.3.

--- pynamodb/_compat.py
+++ pynamodb/_compat.py
@@ -1,8 +1,19 @@
 import six
+import sys
 
 if six.PY2:
     from inspect import getargspec as getfullargspec
 else:
     from inspect import getfullargspec
 
-__all__ = ('getfullargspec',)
+def load_module(name, path):
+    """Load module using the Python version compatible function."""
+    if sys.version_info >= (3, 3):
+        from imp import load_source
+        from importlib.machinery import SourceFileLoader
+        return SourceFileLoader(name, path).load_module()
+    else: 
+        from imp import load_source
+        return load_source(name, path)
+
+__all__ = ('getfullargspec', 'load_module')
