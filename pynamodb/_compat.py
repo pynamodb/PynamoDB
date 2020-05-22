6d70d5a4e9f6921d3b3504c8f2082beea507a221
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2020-05-22 10:11:59 -0400

    clean up compat

--- pynamodb/_compat.py
+++ pynamodb/_compat.py
@@ -1,12 +1,7 @@
-import sys
-
-from inspect import getfullargspec
-
-
 class FakeGenericMeta(type):
     """Poor man's Generic[T] that doesn't depend on typing. The real generics are in the type stubs."""
     def __getitem__(self, item):
         return self
 
 
-__all__ = ('getfullargspec', 'FakeGenericMeta')
+__all__ = ('FakeGenericMeta',)
