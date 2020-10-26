2444c0d0c91278d9359c0587d4cdd29702d480c8
Ryan S. Brown <sb@ryansb.com>
2020-10-25 20:48:34 -0400

    Add integration tests

--- tests/integration/binary_update_test.py
+++ tests/integration/binary_update_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pynamodb.attributes import UnicodeAttribute, BinaryAttribute, BinarySetAttribute
+from pynamodb.attributes import UnicodeAttribute, BinaryAttribute, BinarySetAttribute, CompressedAttribute
 from pynamodb.models import Model
 
 
@@ -23,6 +23,23 @@ def test_binary_attribute_update(ddb_url):
     new_data = b'\xff'
     m.update(actions=[DataModel.data.set(new_data)])
     assert new_data == m.data
+
+@pytest.mark.ddblocal
+def test_compressed_attribute_writes_large_items(ddb_url):
+    class DataModel(Model):
+        class Meta:
+            table_name = 'compressed_attr_update'
+            host = ddb_url
+        pkey = UnicodeAttribute(hash_key=True)
+        data = CompressedAttribute()
+
+    DataModel.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+    # create a 1MB+ string of bytes to compress and put in the DB
+    data = bytes('1234567890' * 1024 * 1024, 'utf-8')
+    pkey = 'pkey'
+    DataModel(pkey, data=data).save()
+    m = DataModel.get(pkey)
+    assert m.data == data
 
 @pytest.mark.ddblocal
 def test_binary_set_attribute_update(ddb_url):
