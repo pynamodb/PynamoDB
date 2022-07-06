14440d74a357a68d35f3734a433d33a576bcecb0
Etty <28976199+estyxx@users.noreply.github.com>
2022-07-06 15:13:42 +0100

    Add explicit export to connection

--- pynamodb/connection/__init__.py
+++ pynamodb/connection/__init__.py
@@ -4,3 +4,6 @@ PynamoDB lowest level connection
 
 from pynamodb.connection.base import Connection
 from pynamodb.connection.table import TableConnection
+
+
+__all__ = ["Connection", "TableConnection"]
