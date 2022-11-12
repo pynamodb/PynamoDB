927df864210ffc9c4caaab48a6b70068e00a5e3f
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2022-11-12 08:08:24 -0500

    Remove unused assert_mypy_output (#1111)

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def assert_mypy_output(pytestconfig):
-    pytest.register_assert_rewrite('tests.mypy_helpers')
-
-    from tests.mypy_helpers import assert_mypy_output
-    return lambda program: assert_mypy_output(program, use_pdb=pytestconfig.getoption('usepdb'))
