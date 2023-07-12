b13dc6e6d2846f6fbd55dba59201dddac6af7e01
Ilya Priven <ilya.konstantinov@gmail.com>
2023-07-11 21:49:59 -0700

    Fix updating set to empty

--- pynamodb/expressions/operand.py
+++ pynamodb/expressions/operand.py
@@ -298,9 +298,12 @@ class Path(_NumericOperand, _ListAppendOperand, _ConditionOperand):
     def __or__(self, other):
         return _IfNotExists(self, self._to_operand(other))
 
-    def set(self, value: Any) -> SetAction:
+    def set(self, value: Any) -> Union[SetAction, RemoveAction]:
         # Returns an update action that sets this attribute to the given value
-        return SetAction(self, self._to_operand(value))
+        operand = self._to_operand(value)
+        if isinstance(operand, Value) and next(iter(operand.value.values())) is None:
+            return RemoveAction(self)
+        return SetAction(self, operand)
 
     def remove(self) -> RemoveAction:
         # Returns an update action that removes this attribute from the item
